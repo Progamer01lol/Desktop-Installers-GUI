394bddd01a598284008c427abe224bae21c7149e
Progamer01lol <paytonp061023@gmail.com>
2026-06-11 18:21:43 -0600

    Update desktop-installer.sh

--- desktop-installer.sh
+++ desktop-installer.sh
@@ -1,4 +1,4 @@
-CHOICE=$(zenity --list --title="Choose a Desktop" --column="Desktop Environments" "budgie" "cinnamon" "cosmic" "deepin" "enlightenment" "gnome" "gnome-flashback" "plasma" "plasma-mobile" "lxde" "lxqt" "mate" "pantheon" "phosh" "sugar" "xfce4")
+CHOICE=$(zenity --list --title="Choose a Desktop" --column="Desktop Environments" "budgie" "cinnamon" "cosmic" "deepin" "enlightment" "gnome" "gnome-flashback" "plasma" "plasma-mobile" "lxde" "lxqt" "mate" "pantheon" "phosh" "sugar" "xfce4")
     case $CHOICE in
         "budgie")
             sudo pacman -S budgie --noconfirm
