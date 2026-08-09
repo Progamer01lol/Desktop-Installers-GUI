b5b2d6afdadadfb87ec7d92ec127249eac3e17a2
Progamer01lol <paytonp061023@gmail.com>
2026-08-09 14:02:33 -0600

    More GUI Fixes

--- desktop-installer.sh
+++ desktop-installer.sh
@@ -1,52 +1,53 @@
+pkexec pacman -S zenity --noconfirm
 CHOICE=$(zenity --list --title="Choose a Desktop" --column="Desktop Environments" "budgie" "cinnamon" "cosmic" "deepin" "enlightenment" "gnome" "gnome-flashback" "plasma" "plasma-mobile" "lxde" "lxqt" "mate" "pantheon" "phosh" "sugar" "xfce4")
     case $CHOICE in
         "budgie")
-            sudo pacman -S budgie --noconfirm
+            pkexec pacman -S budgie --noconfirm
             ;;
         "cinnamon")
-            sudo pacman -S cinnamon --noconfirm
+            pkexec pacman -S cinnamon --noconfirm
             ;;
         "cosmic")
-            sudo pacman -S cosmic --noconfirm
+            pkexec pacman -S cosmic --noconfirm
             ;;
         "deepin")
-            sudo pacman -S deepin --noconfirm
+            pkexec pacman -S deepin --noconfirm
             ;;
         "enlightenment")
-            sudo pacman -S enlightenment --noconfirm
+            pkexec pacman -S enlightenment --noconfirm
             ;;
         "gnome")
-            sudo pacman -S gnome --noconfirm
+            pkexec pacman -S gnome --noconfirm
             ;;
         "gnome-flashback")
-            sudo pacman -S gnome-flashback --noconfirm
+            pkexec pacman -S gnome-flashback --noconfirm
             ;;
         "plasma")
-            sudo pacman -S plasma --noconfirm
+            pkexec pacman -S plasma --noconfirm
             ;;
         "plasma-mobile")
-            sudo pacman -S plasma-mobile --noconfirm
+            pkexec pacman -S plasma-mobile --noconfirm
             ;;
         "lxde")
-            sudo pacman -S lxde --noconfirm
+            pkexec pacman -S lxde --noconfirm
             ;;
         "lxqt")
-            sudo pacman -S lxqt --noconfirm
+            pkexec pacman -S lxqt --noconfirm
             ;;
         "mate")
-            sudo pacman -S mate --noconfirm
+            pkexec pacman -S mate --noconfirm
             ;;
         "pantheon")
-            sudo pacman -S pantheon --noconfirm
+            pkexec pacman -S pantheon --noconfirm
             ;;
         "phosh")
-            sudo pacman -S phosh --noconfirm
+            pkexec pacman -S phosh --noconfirm
             ;;
         "sugar")
-            sudo pacman -S sugar --noconfirm
+            pkexec pacman -S sugar --noconfirm
             ;;
         "xfce4")
-            sudo pacman -S xfce4 --noconfirm
+            pkexec pacman -S xfce4 --noconfirm
             ;;
         *)
             zenity --error --text="Invalid option, please try again."
